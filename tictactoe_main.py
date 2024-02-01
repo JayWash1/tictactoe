f7db01ee4d3e657b814733e64ea611c67e004277
JayWash1 <122568234+JayWash1@users.noreply.github.com>
2024-01-31 21:23:27 -0500

    Added ability to select position

--- tictactoe_main.py
+++ tictactoe_main.py
@@ -1,3 +1,6 @@
+from helpers import print_board, clear_screen
+
+
 #welcome message
 print(""" 
       =======================
@@ -13,24 +16,60 @@ print("""
       Keep your wits about you, strategize your moves, and aim to outsmart your opponent in this timeless game of Tic-Tac-Toe!
       """)
 
-#print game board
-#game board has 9 positions
-def print_board(board):
-    print("\n")
-    for row in board:
-        print(" | ".join(row))
-        print("-" * 9)
-    print("\n")
-
+#Display numbered board to show 9 positions
 board = [
     ["1", "2", "3"],
     ["4", "5", "6"],
     ["7", "8", "9"]
 ]
 
+#print demo game board
 print_board(board)
 
-#take player input
+print("The board has 9 positions. Choose your positiona according to the board above.\n")
+
+#clear board
+board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "]
+]
+
+input("Press Enter to continue...")
+clear_screen()
+
+#Initialize game
+game_on = True
+
+while game_on:
+    #Display game board
+    print_board(board)
+    #Get input from player
+    choice = input("Choose position: ")
+    if choice == "1":
+        board[0][0] = "X"
+    if choice == "2":
+        board[0][1] = "X"
+    if choice == "3":
+        board[0][2] = "X"
+    if choice == "4":
+        board[1][0] = "X"
+    if choice == "5":
+        board[1][1] = "X"
+    if choice == "6":
+        board[1][2] = "X"
+    if choice == "7":
+        board[2][0] = "X"
+    if choice == "8":
+        board[2][1] = "X"
+    if choice == "9":
+        board[2][2] = "X"
+    else:
+        game_on = False
+
+#board[0][0] = "X"
+
+print_board(board)
 
 #player_1 is x and player_2 is o
 
